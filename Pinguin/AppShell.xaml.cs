5eba17cab79b8076c92c3cec0511fcf1590e5d64
Oleksii Kolesnyk <oleksii.kolesnyk.w@gmail.com>
2024-12-01 00:10:58 +0200

    Init

---
+++ Pinguin/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Pinguin;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Pinguin/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Pinguin;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
